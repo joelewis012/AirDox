fee9b3c6f83aa3a49f9a4f6a5d16d33c40a93e40
joelewis012 <joelewis_1996@icloud.com>
2025-12-25 04:49:05 +0000

    Update apple_ble_hash_demo.h

--- applications_user/air_dox/apple_ble_hash_demo.h
+++ applications_user/air_dox/apple_ble_hash_demo.h
@@ -1,10 +1,3 @@
 #pragma once
-#include <stdint.h>
-#include <stddef.h>
 
-typedef struct {
-    uint8_t hash[2];
-    char phone[17];
-} phone_hash_lookup_t;
-
-int32_t apple_ble_hash_demo_app(void* p);
+void apple_ble_hash_demo_init(void);
