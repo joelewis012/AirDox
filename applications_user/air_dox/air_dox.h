79051e302a6d597fa187575447714eadf2550bc2
joelewis012 <joelewis_1996@icloud.com>
2025-12-25 04:46:24 +0000

    Create air_dox.h

---
+++ applications_user/air_dox/air_dox.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <furi.h>
+#include <gui/gui.h>
+#include <input/input.h>
+
+void airdox_draw_callback(Canvas* canvas, void* ctx);
+void airdox_input_callback(InputEvent* input_event, void* ctx);
